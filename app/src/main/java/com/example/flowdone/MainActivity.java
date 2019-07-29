1be064c9d49141edf12d8edd6f2d6c1415a3f27f
jamil-zaitouny <39029020+jamil-zaitouny@users.noreply.github.com>
2019-07-29 09:23:28 +0300

    signUp button temp function
    Adds a temporary function notifying the user that sign up will be added at a later date

--- app/src/main/java/com/example/flowdone/MainActivity.java
+++ app/src/main/java/com/example/flowdone/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.flowdone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mSignUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Tells the user that sign up will be made available at a later date
+        mSignUpButton = (Button)findViewById(R.id.signUp);
+        mSignUpButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Sign up will become available at a later date!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
