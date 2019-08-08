7b56f05473dd444bfea00b69744b37daa4efd779
jamil-zaitouny <39029020+jamil-zaitouny@users.noreply.github.com>
2019-08-07 22:18:30 -0700

    Revert "Added notifications and add button float dialog"
    This reverts commit 55a225d43c400f4493ea60a1ac5f56002b21a350.

--- app/src/main/java/com/example/flowdone/MainActivity.java
+++ app/src/main/java/com/example/flowdone/MainActivity.java
@@ -8,7 +8,6 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -37,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.i("debbug","got here");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FirebaseApp.initializeApp(this);
